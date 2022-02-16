222e1002b33667861621d01530c61ecca0ceed2f
Amy Berish <aberish@rockarch.org>
2022-02-16 17:14:14 -0500

    add test_validator function to load JSON data

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -2,6 +2,21 @@
 #
 # from dacsspace.validator import Validator
 
+import json
+import unittest
 
-def test_validator():
-    pass
+# HELP: I noticed in other tests there is usually a class, so I added one, but
+# I don't know if this is necessary
+
+
+class TestValidator(unittest.TestCase):
+    def test_validator():
+        json_file = "/Users/aberish/Documents/GitHub/DACSspace/fixtures/resource.json"
+        with open(json_file, 'r') as f:
+            json_data = json.load(f)
+        return(json_data)
+
+
+# HELP: I don't know what this does, but I saw it used in other tests
+if __name__ == "__main__":
+    unittest.main()
