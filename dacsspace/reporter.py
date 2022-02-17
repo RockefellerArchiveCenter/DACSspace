34393f0ac7172039fc6c4bdc519678aa3870678b
daremiyo <dyoung@rockarch.org>
2022-02-17 11:55:49 -0500

    start of logic for creating the csv report

--- dacsspace/reporter.py
+++ dacsspace/reporter.py
@@ -1,9 +1,13 @@
+import csv
+
 
 class CSVReporter:
     """Creates CSV reports."""
 
-    def __init__(self):
+    def __init__(self, filename):
         # TODO: set filepath for CSV
+        self.filename = filename
+
         pass
 
     def write_report(self, results, invalid_only=True):
@@ -13,4 +17,21 @@ class CSVReporter:
             results (list): A list of dictionaries containing information about validation results.
             invalid_only (boolean): Only report on invalid results.
         """
+
+        with open(self.filename) as f:
+            fieldnames = [
+                "title",
+                "publish",
+                "resource",
+                "extent",
+                "date",
+                "language",
+                "repository",
+                "creator",
+                "scope",
+                "restrictions"]
+            writer = csv.DictWriter(
+                f, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(results)
         pass
