c940660adb8067ba4562307bc25888567521eb7c
Amy Berish <aberish@rockarch.org>
2022-02-23 15:33:00 -0500

    import validator class, return json and validate

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,22 +1,22 @@
 # from unittest.mock import patch
-#
-# from dacsspace.validator import Validator
+
 
 import json
 import unittest
 
-# HELP: I noticed in other tests there is usually a class, so I added one, but
-# I don't know if this is necessary
+from dacsspace.validator import Validator
 
 
 class TestValidator(unittest.TestCase):
-    def test_validator():
+    def test_validator(self):
         json_file = "/Users/aberish/Documents/GitHub/DACSspace/fixtures/resource.json"
         with open(json_file, 'r') as f:
             json_data = json.load(f)
-        return json_data
+            result = Validator().validate_data(json_data)
+        self.assertTrue(isinstance(result, dict))
+        self.assertEqual(result["valid"], "True")
 
 
 # HELP: I don't know what this does, but I saw it used in other tests
-if __name__ == "__main__":
-    unittest.main()
+# if __name__ == "__main__":
+#    unittest.main()
