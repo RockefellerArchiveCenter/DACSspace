d88df2da0af2fb7173f32998dbf5cfd9141c0f6a
Amy Berish <aberish@rockarch.org>
2022-03-03 16:41:58 -0500

    try to test against invalid json

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -9,9 +9,15 @@ from dacsspace.validator import Validator
 
 class TestValidator(unittest.TestCase):
     def test_validator(self):
-        json_file = "/Users/aberish/Documents/GitHub/DACSspace/fixtures/resource.json"
-        with open(json_file, 'r') as f:
-            json_data = json.load(f)
-            result = Validator().validate_data(json_data)
-        self.assertTrue(isinstance(result, dict))
-        self.assertEqual(result["valid"], "True")
+        # valid_json = "/Users/aberish/Documents/GitHub/DACSspace/fixtures/valid_resource.json"
+        invalid_json = "/Users/aberish/Documents/GitHub/DACSspace/fixtures/invalid_resource.json"
+        with open(invalid_json, 'r') as f:
+            invalid_json = json.load(f)
+            result = Validator().validate_data(invalid_json)
+        self.assertFalse(isinstance(result, dict))
+        self.assertEqual(result["valid"], False)
+        # with open(invalid_json, 'r') as i:
+        #     invalid_json = json.load(i)
+        #     result = Validator().validate_data(invalid_json)
+        # self.assertFalse(isinstance(result, dict))
+        # self.assertEqual(result["valid"], False)
