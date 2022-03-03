4419f86b9252cea4d7c5f78c1f71489dbb6c37aa
Amy Berish <aberish@rockarch.org>
2022-03-03 17:34:25 -0500

    add testing against valid fixture

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -9,15 +9,15 @@ from dacsspace.validator import Validator
 
 class TestValidator(unittest.TestCase):
     def test_validator(self):
-        # valid_json = "/Users/aberish/Documents/GitHub/DACSspace/fixtures/valid_resource.json"
-        invalid_json = "/Users/aberish/Documents/GitHub/DACSspace/fixtures/invalid_resource.json"
-        with open(invalid_json, 'r') as f:
-            invalid_json = json.load(f)
+        valid_json = "fixtures/valid_resource.json"
+        invalid_json = "fixtures/invalid_resource.json"
+        with open(valid_json, 'r') as v:
+            valid_json = json.load(v)
+            result = Validator().validate_data(valid_json)
+        self.assertTrue(isinstance(result, dict))
+        self.assertEqual(result["valid"], True)
+        with open(invalid_json, 'r') as i:
+            invalid_json = json.load(i)
             result = Validator().validate_data(invalid_json)
         self.assertTrue(isinstance(result, dict))
         self.assertEqual(result["valid"], False)
-        # with open(invalid_json, 'r') as i:
-        #     invalid_json = json.load(i)
-        #     result = Validator().validate_data(invalid_json)
-        # self.assertFalse(isinstance(result, dict))
-        # self.assertEqual(result["valid"], False)
