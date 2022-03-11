ff74a8c2a085d9e079be4decfa52c2ed3ab8a739
Amy Berish <aberish@rockarch.org>
2022-03-11 12:46:30 -0500

    rename json file

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -10,14 +10,14 @@ from dacsspace.validator import Validator
 class TestValidator(unittest.TestCase):
     def test_validator(self):
         valid_json = "fixtures/valid_resource.json"
-        invalid_json = "fixtures/invalid_resource.json"
+        multiple_invalid = "fixtures/invalid_resource.json"
         with open(valid_json, 'r') as v:
             valid_json = json.load(v)
             result = Validator().validate_data(valid_json)
         self.assertTrue(isinstance(result, dict))
         self.assertEqual(result["valid"], True)
-        with open(invalid_json, 'r') as i:
-            invalid_json = json.load(i)
-            result = Validator().validate_data(invalid_json)
+        with open(multiple_invalid, 'r') as i:
+            multiple_invalid = json.load(i)
+            result = Validator().validate_data(multiple_invalid)
         self.assertTrue(isinstance(result, dict))
         self.assertEqual(result["valid"], False)
