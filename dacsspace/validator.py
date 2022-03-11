e8583ceb18112d6e308fd0cb39741414ad9cbeff
Amy Berish <aberish@rockarch.org>
2022-03-11 12:31:37 -0500

    remove comments

--- dacsspace/validator.py
+++ dacsspace/validator.py
@@ -4,7 +4,6 @@ from jsonschema import ValidationError, validate
 
 class Validator:
     """Validates data from ArchivesSpace."""
-# Creates a simple schema to validate json against
 
     def get_schema(self):
         schema = {
@@ -22,7 +21,6 @@ class Validator:
         }
         return schema
 
-# Checks data against schema
     def validate_data(self, data):
         """Validates data.
 
@@ -35,16 +33,10 @@ class Validator:
            of any validation errors. { "valid": False, "explanation": "You are missing the following fields..." }
         """
         schema = self.get_schema()
-        # print(data)
         try:
             validate(data, schema)
             return {"valid": True}
         except ValidationError as error:
-            # print(error.message)
             return {"valid": False, "explanation": error.message}
-            # continue
         else:
             print("Something else is wrong")
-
-
-# Validator.validate_data(json_data, schema)
