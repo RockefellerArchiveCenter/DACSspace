c2c874345aa70fbcf40e3ff9b2760ba2e5264943
bonniegee <bgordon@rockarch.org>
2022-03-15 16:39:14 -0400

    add initial schema

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -10,14 +10,15 @@ from dacsspace.validator import Validator
 class TestValidator(unittest.TestCase):
     def test_validator(self):
         valid_json = "fixtures/valid_resource.json"
-        multiple_invalid = "fixtures/invalid_resource.json"
+        invalid_fixtures = ["fixtures/multiple_invalid.json", "fixtures/no_metadata_rights.json"]
         with open(valid_json, 'r') as v:
             valid_json = json.load(v)
             result = Validator().validate_data(valid_json)
         self.assertTrue(isinstance(result, dict))
         self.assertEqual(result["valid"], True)
-        with open(multiple_invalid, 'r') as i:
-            multiple_invalid = json.load(i)
-            result = Validator().validate_data(multiple_invalid)
-        self.assertTrue(isinstance(result, dict))
-        self.assertEqual(result["valid"], False)
+        for f in invalid_fixtures:
+            with open(f, 'r') as i:
+                invalid_json = json.load(i)
+                result = Validator().validate_data(invalid_json)
+            self.assertTrue(isinstance(result, dict))
+            self.assertEqual(result["valid"], False)
