0eb4e4343407ae67e912953870596dd3125a8ffd
bonniegee <bgordon@rockarch.org>
2022-03-29 16:30:05 -0400

    move fixtures to subdirectories

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,26 +1,31 @@
-# from unittest.mock import patch
-
-
 import json
 import unittest
+from pathlib import Path
 
 from dacsspace.validator import Validator
+
+INVALID_FIXTURE_DIRECTORY = Path("fixtures", "invalid")
+VALID_FIXTURE_DIRECTORY = Path("fixtures", "valid")
 
 
 class TestValidator(unittest.TestCase):
     def test_validator(self):
-        valid_json = "fixtures/valid_resource.json"
+        valid_fixtures = [
+            f for f in VALID_FIXTURE_DIRECTORY.iterdir() if (
+                VALID_FIXTURE_DIRECTORY.joinpath(f).is_file() and str(
+                    f.name).endswith("json"))]
+        for valid in valid_fixtures:
+            with open(valid, 'r') as v:
+                valid_json = json.load(v)
+                result = Validator().validate_data(valid_json)
+            self.assertTrue(isinstance(result, dict))
+            self.assertEqual(result["valid"], True)
         invalid_fixtures = [
-            "fixtures/multiple_invalid.json",
-            "fixtures/no_metadata_rights.json",
-            "fixtures/no_accessrestrict.json"]
-        with open(valid_json, 'r') as v:
-            valid_json = json.load(v)
-            result = Validator().validate_data(valid_json)
-        self.assertTrue(isinstance(result, dict))
-        self.assertEqual(result["valid"], True)
-        for f in invalid_fixtures:
-            with open(f, 'r') as i:
+            f for f in INVALID_FIXTURE_DIRECTORY.iterdir() if (
+                INVALID_FIXTURE_DIRECTORY.joinpath(f).is_file() and str(
+                    f.name).endswith("json"))]
+        for invalid in invalid_fixtures:
+            with open(invalid, 'r') as i:
                 invalid_json = json.load(i)
                 result = Validator().validate_data(invalid_json)
             self.assertTrue(isinstance(result, dict))
