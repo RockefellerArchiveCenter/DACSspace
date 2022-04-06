e6ed3f98b139254226214610cc1ec5c704ce4776
kemartin2015 <k.martin@rockarch.org>
2022-04-06 08:22:12 -0400

    update imports, add schema arguments

--- dacsspace/dacsspace.py
+++ dacsspace/dacsspace.py
@@ -1,16 +1,17 @@
 import argparse
 
-from .client import ArchivesSpaceClient
-from .reporter import CSVReporter
-from .validator import Validator
+from client import ArchivesSpaceClient
+from reporter import CSVReporter
+from validator import Validator
 
 
 class DACSspace:
     """Base DACSspace class. Fetches data from AS, validates and reports results."""
 
-    def run(self, published_only, invalid_only):
+    def run(self, published_only, invalid_only,
+            schema_identifier, schema_filepath):
         client = ArchivesSpaceClient()
-        validator = Validator()
+        validator = Validator(schema_identifier, schema_filepath)
         reporter = CSVReporter()
         data = client.get_resources(published_only)
         results = []
@@ -31,6 +32,19 @@ if __name__ == '__main__':
         '--invalid_only',
         help='Reports only invalid data',
         action='store_true')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        '--schema_identifier',
+        help='Choose schema_identifier or schema_filepath. schema_identifier default is single_level_required.json',
+        type=str, default='single_level_required.json')
+    group.add_argument(
+        '--schema_filepath',
+        help='Choose schema_identifier or schema_filepath. Schema_filepath default is None, only one of schema_identifier',
+        type=str, default=None)
     args = parser.parse_args()
 
-    DACSspace().run(args.published_only, args.invalid_only)
+DACSspace().run(
+    args.published_only,
+    args.invalid_only,
+    args.schema_identifier,
+    args.schema_filepath)
